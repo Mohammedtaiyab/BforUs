54b589634192832fcabde52c5c39a0a4f5ea1a1d
Mohammedtaiyab <55629150+Mohammedtaiyab@users.noreply.github.com>
2021-02-18 16:45:02 +0530

    menu

--- fakhricafe/place.php
+++ fakhricafe/place.php
@@ -1,54 +1,8 @@
 <?php
-// require "PHPMailer/PHPMailerAutoload.php";
-use PHPMailer\src\Exception;
-use PHPMailer\PHPMailer\PHPMailer;
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
-function smtpmailer($to, $from, $from_name, $subject, $body)
-    {
-        $mail = new PHPMailer();
-        $mail->IsSMTP();
-        $mail->SMTPAuth = true; 
- 
-        $mail->SMTPSecure = 'ssl'; 
-        $mail->Host = 'mail.bforus.in';
-        $mail->Port = 465;  
-        $mail->Username = 'info@bforus.in';
-        $mail->Password = 'Bforus@123';   
-   
-   //   $path = 'reseller.pdf';
-   //   $mail->AddAttachment($path);
-   
-        $mail->IsHTML(true);
-        $mail->From="info@bforus.in";
-        $mail->FromName=$from_name;
-        $mail->Sender=$from;
-        $mail->AddReplyTo($from, $from_name);
-        $mail->Subject = $subject;
-        $mail->Body = $body;
-        $mail->AddAddress($to);
-        if(!$mail->Send())
-        {
-            $error ="Please try Later, Error Occured while Processing...";
-            return $error; 
-        }
-        else 
-        {
-            $error = "Thanks You !! Your email is sent.";  
-            return $error;
-        }
-    }
- 
-    $table=$_GET['table'];
-    $to='mohammedbori44@gmail.com';
-    $from="info@bforus.in";
-    $name= "Table No:".$table;
-    $subj = "Order";
-   $msg = "Order Place"; 
-    //$error=smtpmailer($to,$from, $name ,$subj, $msg);
-    if(smtpmailer($to,$from, $name ,$subj, $msg)){
-header('Location:index.html');    }
-    
-    
+/* watch the video for detailed instructions */
+$to = "5555555555@vtext.com";
+$from = "xxxx@yourdomain.com";
+$message = "This is a text message\nNew line...";
+$headers = "From: $from\n";
+mail($to, '', $message, $headers);
 ?>
